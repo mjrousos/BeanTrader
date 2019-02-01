5fa5d68a6bda522dce61e77238483c070eb02fd6
Mike Rousos <Mike@MJRousos.com>
2019-02-01 16:46:01 -0500

    Add initial WPF client scaffold with WCF service reference

---
+++ BeanTraderClient/BeanTraderCallback.cs
@@ -0,0 +1,28 @@
+﻿using BeanTrader;
+using BeanTrader.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeanTraderClient
+{
+    public class BeanTraderCallback : BeanTraderServiceCallback
+    {
+        public void AddNewTradeOffer(TradeOffer offer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveTradeOffer(Guid offerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TradeAccepted(Guid offerId, Guid buyerId, string buyerName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
