68da36c062f3b3349cd4e736972a675c8cf94539
Mike Rousos <Mike@MJRousos.com>
2019-02-09 00:30:59 -0500

    Select text in new trade offer text boxes when they receive focus

--- BeanTraderClient/Controls/NewTradeOfferControl.xaml.cs
+++ BeanTraderClient/Controls/NewTradeOfferControl.xaml.cs
@@ -30,5 +30,26 @@ namespace BeanTraderClient.Controls
 
         private void CancelButton_Click(object sender, RoutedEventArgs e) => Model.CancelTradeOffer();
         private void CreateButton_Click(object sender, RoutedEventArgs e) => Model.CreateTradeOffer();
+
+        // Select text in the bean count text boxes so that users can easily replace the initial '0'
+        private void BeanTextBox_GotFocus(object sender, EventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                textBox.SelectAll();
+            }
+        }
+
+        private void IgnoreIfUnfocused(object sender, MouseEventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                if (!textBox.IsKeyboardFocusWithin)
+                {
+                    textBox.Focus();
+                    e.Handled = true;
+                }
+            }
+        }
     }
 }
