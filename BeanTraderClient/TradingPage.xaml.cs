945eca95a9076f1595772ab2c08c8599d5804fd6
Mike Rousos <Mike@MJRousos.com>
2019-02-08 22:53:18 -0500

    Make sure the trading page is unloaded even on app exit

--- BeanTraderClient/TradingPage.xaml.cs
+++ BeanTraderClient/TradingPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using BeanTraderClient.ViewModels;
 using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
@@ -17,7 +19,18 @@ namespace BeanTraderClient
         {
             InitializeComponent();
             this.DataContext = this.Model;
-            this.Unloaded += (sender, args) => Model.Dispose();
+
+            // Make sure that this page's model is cleaned up if
+            // the page is navigated away from or if the app closes
+            this.Unloaded += Unload;
+            Application.Current.MainWindow.Closing += Unload;
+        }
+
+        private void Unload(object sender, EventArgs e)
+        {
+            Application.Current.MainWindow.Closing -= Unload;
+            Model?.Dispose();
+            Model = null;
         }
 
         private void LogoutButton_Click(object sender, RoutedEventArgs e)
